103a218dd94f3fbb4302c00955d676b7a1d0d58a
miranda-zapata <miranda.zapata@gmail.com>
2022-12-10 15:24:06 +0100

    Add full JavaScript in one sitting (and adapt HTML)
    Añadido todo el JavaScript necesario para añadir transacciones, operar
    con ellas y borrarlas. Edición del HTML para adecuarlo al JS

--- src/main.js
+++ src/main.js
@@ -1,31 +1,103 @@
-/* Variables */
-const balance = document.getElementById('balance__qty');
-const income = document.getElementById('income__qty');
-const expense = document.getElementById('expenses__qty');
+// 'use strict';
+/* Constantes */
+const savings = document.getElementById('savings');
+// Otra forma const savings = document.querySelector('.js-savingsQty');
+const income = document.getElementById('income');
+const outcome = document.getElementById('outcome');
 const list = document.getElementById('records');
-const form = document.getElementById('transactions');
-const transactions = document.getElementById('transactions');
+const concept = document.getElementById('concept');
+const amount = document.getElementById('amount');
+const form = document.getElementById('transactions-form');
 
-/* Añadir transacción */
-transactions.addEventListener('submit', (ev) => {
+/* Variables */
+let transactions = [];
+
+/* Evento para añadir una transacción vía formulario */
+form.addEventListener('submit', (ev) => {
   // Prevenir el evento submit del form
   ev.preventDefault();
 
-  const inputConcept = document.getElementById('concept');
-  const inputAmount = document.getElementById('amount');
-
   let transaction = {
-    concept: inputConcept.value,
-    amount: +inputAmount.value,
+    concept: concept.value,
+    amount: +amount.value,
+    // Podría hacerlo así id: Math.floor(Math.random() * 100000), o con una función:
+    id: getId(),
   };
+  // Añado transacciones al array con el método push
   transactions.push(transaction);
 
-  inputConcept.value = '';
-  inputAmount.value = '';
+  addTransaction(transaction);
+  updateBalance(transaction);
+
+  concept.value = '';
+  amount.value = '';
 });
 
+/* Generar una ID aleatoria */
+function getId() {
+  return Math.floor(Math.random() * 100000);
+}
+
 /* Actualizar balance */
-/* Histórico */
+function updateBalance() {
+  // Método map para crear un array con las cantidades de las transacciones
+  const amounts = transactions.map((transaction) => transaction.amount);
+  // Método reduce para sumar los valores de las transacciones (total)
+  // Balance = bal = dónde quiero acumular
+  // value = qué quiero acumular
+  const total = amounts.reduce((bal, value) => (bal += value), 0).toFixed(2);
+  //  Suma de todos los ingresos (números positivos) del array amounts
+  const earnings = amounts
+    .filter((value) => value > 0)
+    .reduce((bal, value) => (bal += value), 0)
+    .toFixed(2);
+  // Gastos (números negativos)
+  const expenses =
+    amounts
+      .filter((value) => value < 0)
+      .reduce((bal, value) => (bal += value), 0) * -(1).toFixed(2);
+
+  savings.innerHTML = `${total} €`;
+  income.innerHTML = `${earnings} €`;
+  outcome.innerHTML = `${expenses} €`;
+}
+
+/* Añadir transacción */
+function addTransaction(transaction) {
+  // Obetener el tipo de transacción por su signo
+  // Si la cantidad es menor que cero, es negativa. Si es mayor que cero, positiva
+  const sign = transaction.amount < 0 ? '-' : '+';
+  // Variable para los elementos de la lista (ul)
+  const elt = document.createElement('li');
+
+  // Añadimos al elemento (li) una clase basada en su value
+  elt.classList.add(transaction.amount < 0 ? 'negative' : 'positive');
+  // Pintamos dentro de ese elemento (li) el número absoluto con su signo
+  elt.innerHTML = `<i id="delete" role="button">❌</i> <span>${
+    transaction.concept
+  }</span>, <span>${sign}${Math.abs(transaction.amount)}</span>`;
+
+  // A nuestra lista (ul) le ponemos un elemento (li)
+  list.appendChild(elt);
+
+  // Listener del icono para eliminar transacción (usando su primer hijo, el i)
+  elt.firstElementChild.addEventListener('click', () => {
+    removeTransaction(transaction.id);
+  });
+}
+
 /* Eliminar transacción */
+function removeTransaction(id) {
+  transactions = transactions.filter((transaction) => transaction.id !== id);
+
+  startApp();
+}
+
 /* Inicializar app */
-/* Eventos */
+function startApp() {
+  list.innerHTML = '';
+  transactions.forEach(addTransaction);
+  updateBalance();
+}
+
+startApp();
